626ca76d7e073565da263767736c9dd26d5d21b1
whussain-mm <w.hussain@pk.micromerger.com>
2025-07-04 15:05:35 +0500

    feat: Add mail and phone icons to footer, enhance layout and styling for contact information

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import footerlogo from '../../../../assets/images/lsuhs-logo.png';
+import footerlogo from '../assets/images/lsuhs-logo.png';
+import mailIcon from '../assets/icons/mail-icon.svg';
+import phoneIcon from '../assets/icons/phone-icon.svg';
 import './Footer.css';
+import { Row, Col } from 'antd';
 
 const Footer = () => {
   return (
@@ -8,14 +11,43 @@ const Footer = () => {
     <footer className="footer">
       <div className="container">
         <div className="footer-header">
-            <div className="footer-logo">
-                            <img src={footerlogo} alt="footerlogo" />
-                          </div>
-          <h2 className="footer-title">LSUHS</h2>
-          <p className="footer-subtitle">Louisiana State University Health Sciences</p>
+            <Row gutter={16} align="middle" justify="space-between">
+                <Col span={8} md={8}>
+                <div className="footer-logo">
+                    <img src={footerlogo} alt="footerlogo" />
+                </div>
+                </Col>
+                <Col span={16} md={16}>
+                    <div className="d-flex media-flex justify-content-start align-items-center"         >
+                        <h5>Contact</h5>
+                        
+                            <a href="#"  className='media-link'>
+                                <span className='icon'><img src={phoneIcon} alt="Phone" /></span>
+                                <span className='text'>225-123-4567</span>
+                            </a>
+                            <a href="#"  className='media-link'>
+                                <span className='icon'><img src={mailIcon} alt="Mail" /></span>
+                                <span className='text'>info@lsuhealth.com</span>
+                            </a>
+                    </div>
+                </Col>
+            </Row>
         </div>
         <div className="footer-content">
-          <p>&copy; 2023 LSUHS. All rights reserved.</p>
+            <Row gutter={16} align="middle" justify="space-between">
+                <Col span={8} md={8}>
+                    <p>&copy; 2023 LSUHS. All rights reserved.</p>
+                </Col>
+                <Col span={16} md={16}>
+                <div className="footer-links">
+                    <ul className="d-flex justify-content-start align-items-center">
+                        <li><a href="#">User Policy</a></li>
+                        <li><a href="#">Terms of Service</a></li>
+                    </ul>
+                </div>
+                </Col>
+            </Row>
+          
         </div>
       </div>
     </footer>
